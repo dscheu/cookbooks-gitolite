84f6a0c0da6935bfa7735827160df31780fe4cd5
dscheu <daniel@dash.sh>
2012-12-09 20:18:35 +0100

    Initialized gitolite cookbook

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Daniel Scheu"
+maintainer_email "daniel@dash.sh"
+license          "GNU Public License 3.0"
+description      "Installs/Configures gitolite"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
